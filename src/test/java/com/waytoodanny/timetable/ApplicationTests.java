0ca9b747bed03a7361f49f06528c00db46cbc288
Daniel Volnitsky <waytodanny@gmail.com>
2020-02-29 20:44:00 +0200

    turn to spring boot project in order to have flexible application configurations

---
+++ src/test/java/com/waytoodanny/timetable/ApplicationTests.java
@@ -0,0 +1,12 @@
+package com.waytoodanny.timetable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+}
