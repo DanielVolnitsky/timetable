2db7c902d9ece54e0f3ab610fe892a8f9a99a50d
Daniel Volnitsky <waytodanny@gmail.com>
2020-04-11 13:24:20 +0300

    add initial population generation hook

--- src/main/java/com/waytoodanny/timetable/configuration/GeneticsConfiguration.java
+++ src/main/java/com/waytoodanny/timetable/configuration/GeneticsConfiguration.java
@@ -1,19 +1,31 @@
 package com.waytoodanny.timetable.configuration;
 
+import com.waytoodanny.timetable.service.generation.TimetableGenerator;
+import com.waytoodanny.timetable.service.generation.genetics.GeneticsTimetableGenerator;
+import com.waytoodanny.timetable.service.generation.genetics.PopulationProvider;
 import com.waytoodanny.timetable.service.generation.genetics.constraint.HardConstraint;
 import com.waytoodanny.timetable.service.generation.genetics.constraint.ScheduleConstraint;
 import com.waytoodanny.timetable.service.generation.genetics.constraint.SoftConstraint;
+import com.waytoodanny.timetable.service.generation.genetics.entity.Chromosome;
 import com.waytoodanny.timetable.service.generation.genetics.entity.FitnessFunction;
+import com.waytoodanny.timetable.service.generation.genetics.entity.Population;
+import com.waytoodanny.timetable.service.generation.genetics.factory.NextGenerationParentsProviderFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
 import java.util.Collection;
+import java.util.Comparator;
+import java.util.Map;
 import java.util.Random;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
 
 @Configuration
 @EnableConfigurationProperties(GeneticsProperties.class)
+@Slf4j
 public class GeneticsConfiguration {
 
   @Bean
@@ -32,12 +44,35 @@ public class GeneticsConfiguration {
         fitnessFunction = updatedFitness;
       }
 
-      //TODO temporary disabled. Restore afterwards
-//      for (SoftConstraint soft : softConstraints) {
-//        fitnessFunction = soft.fitness(chromosome, fitnessFunction);
-//      }
+      for (SoftConstraint soft : softConstraints) {
+        fitnessFunction = soft.fitness(chromosome, fitnessFunction);
+      }
 
       return fitnessFunction;
+    };
+  }
+
+  @Bean
+  public TimetableGenerator timetableGenerator(PopulationProvider pp,
+                                               NextGenerationParentsProviderFactory pf,
+                                               Collection<Consumer<Population>> initPopulationHooks) {
+
+    var gen = new GeneticsTimetableGenerator(pp, pf);
+    initPopulationHooks.forEach(gen::onInitialPopulationGenerated);
+    return gen;
+  }
+
+  @Bean
+  public Consumer<Population> printHook() {
+    return p -> {
+      Map<Integer, Long> m = p.stream()
+          .collect(Collectors.groupingBy(Chromosome::fitnessValue, Collectors.counting()));
+
+      log.info("Initial population stats: " + m);
+
+      m.keySet().stream()
+          .max(Comparator.comparingInt(f -> f))
+          .ifPresent(max -> log.info("Initial population max fitness: " + max));
     };
   }
 
--- src/main/java/com/waytoodanny/timetable/service/generation/genetics/GeneticsTimetableGenerator.java
+++ src/main/java/com/waytoodanny/timetable/service/generation/genetics/GeneticsTimetableGenerator.java
@@ -6,24 +6,34 @@ import com.waytoodanny.timetable.service.generation.TimetableGenerator;
 import com.waytoodanny.timetable.service.generation.genetics.entity.Parents;
 import com.waytoodanny.timetable.service.generation.genetics.entity.Population;
 import com.waytoodanny.timetable.service.generation.genetics.factory.NextGenerationParentsProviderFactory;
-import lombok.Value;
-import org.springframework.stereotype.Component;
+import lombok.RequiredArgsConstructor;
+
+import java.util.function.Consumer;
 
 /**
  * 1. Initialize starting population.
  * Population is presented by the set of chromosome. Each chromosome has a fitness value.
  * Each chromosome consists of genes.
  */
-@Value
-@Component
+@RequiredArgsConstructor
 public class GeneticsTimetableGenerator implements TimetableGenerator {
 
-  PopulationProvider populationProvider;
-  NextGenerationParentsProviderFactory nextGenParentsProviderFactory;
+  private final PopulationProvider populationProvider;
+  private final NextGenerationParentsProviderFactory nextGenParentsProviderFactory;
+
+  private Consumer<Population> onInitialPopulationGenerated = p -> {
+  };
+
+  public GeneticsTimetableGenerator onInitialPopulationGenerated(Consumer<Population> hook) {
+    this.onInitialPopulationGenerated = this.onInitialPopulationGenerated.andThen(hook);
+    return this;
+  }
 
   @Override
   public Timetable timetable(InputData input) {
     Population initialPopulation = populationProvider.population(input);
+    onInitialPopulationGenerated.accept(initialPopulation);
+
     NextGenerationParentsProvider parentsProvider = nextGenParentsProviderFactory.apply(initialPopulation);
 
     Parents parents = parentsProvider.get();
