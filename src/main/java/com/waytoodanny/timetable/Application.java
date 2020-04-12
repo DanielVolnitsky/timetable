e54c1bcfcf2702f82f9108f2e8a602496eb49a83
Daniel Volnitsky <waytodanny@gmail.com>
2020-04-12 20:55:59 +0300

    enhance test example

--- src/main/java/com/waytoodanny/timetable/Application.java
+++ src/main/java/com/waytoodanny/timetable/Application.java
@@ -29,44 +29,40 @@ public class Application implements CommandLineRunner {
   @Override
   public void run(String... args) {
     var rooms = AvailableRooms.of(
-        new Room("1", 10),
+        new Room("1", 20),
         new Room("2", 20));
 
     var subject1 = new Subject("S1");
     var subject2 = new Subject("S2");
+    var subject3 = new Subject("S3");
 
     var teacher1 = Teacher.builder().name("T1").build();
     var teacher2 = Teacher.builder().name("T2").build();
 
     var group1 = new StudentGroup("SG1", 11);
     var group2 = new StudentGroup("SG2", 10);
+    var group3 = new StudentGroup("SG3", 10);
 
     var class1 = TeachingClass.builder()
-        .subject(subject1)
-        .teacher(teacher1)
-        .group(group1)
-        .classesPerWeek(2)
+        .teacher(teacher1).subject(subject1).group(group1).classesPerWeek(4)
         .build();
     var class2 = TeachingClass.builder()
-        .subject(subject1)
-        .teacher(teacher1)
-        .group(group2)
-        .classesPerWeek(1)
+        .teacher(teacher1).subject(subject1).group(group2).classesPerWeek(4)
         .build();
     var class3 = TeachingClass.builder()
-        .subject(subject2)
-        .teacher(teacher2)
-        .group(group1)
-        .classesPerWeek(1)
+        .teacher(teacher1).subject(subject3).group(group1).classesPerWeek(4)
         .build();
     var class4 = TeachingClass.builder()
-        .subject(subject2)
-        .teacher(teacher2)
-        .group(group2)
-        .classesPerWeek(2)
+        .teacher(teacher1).subject(subject3).group(group2).classesPerWeek(4)
+        .build();
+    var class5 = TeachingClass.builder()
+        .teacher(teacher2).subject(subject2).group(group1).classesPerWeek(4)
+        .build();
+    var class6 = TeachingClass.builder()
+        .teacher(teacher2).subject(subject2).group(group2).classesPerWeek(4)
         .build();
 
-    var input = new InputData(Set.of(class1, class2, class3, class4), rooms);
+    var input = new InputData(Set.of(class1, class2, class3, class4, class5, class6), rooms);
 
     Timetable timetable = timetableGenerator.timetable(input);
   }
