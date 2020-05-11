66712372d25ff0bb5361682e571f0a9083b157c8
Daniel <waytodanny@gmail.com>
2020-05-11 13:46:21 +0300

    improve json serialization flexibility

--- src/main/java/com/waytoodanny/timetable/service/TimetableToJsonConverter.java
+++ src/main/java/com/waytoodanny/timetable/service/TimetableToJsonConverter.java
@@ -1,21 +1,115 @@
 package com.waytoodanny.timetable.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.waytoodanny.timetable.Timetable;
 import com.waytoodanny.timetable.TimetableConverter;
+import com.waytoodanny.timetable.genetic.domain.Weekday;
+import com.waytoodanny.timetable.genetic.domain.university.ClassType;
+import com.waytoodanny.timetable.genetic.domain.university.Subject;
+import com.waytoodanny.timetable.genetic.domain.university.Teacher;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.SneakyThrows;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalTime;
+import java.util.List;
+import java.util.Set;
+
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.toSet;
 
 @Component
 @AllArgsConstructor
 public class TimetableToJsonConverter implements TimetableConverter<String> {
 
-  private final ObjectMapper jsonMapper;
+    private final ObjectMapper jsonMapper;
 
-  @Override
-  @SneakyThrows
-  public String convert(Timetable timetable) {
-    return jsonMapper.writeValueAsString(timetable);
-  }
+    @Override
+    @SneakyThrows
+    public String convert(com.waytoodanny.timetable.Timetable timetable) {
+        return jsonMapper.writeValueAsString(
+                Timetable.fromDomain(timetable));
+    }
+
+    @Data
+    private static class Timetable {
+        private final List<TeachingSlot> teachingSlots;
+
+        private static Timetable fromDomain(com.waytoodanny.timetable.Timetable domain) {
+            return new Timetable(
+                    domain.getTeachingSlots().stream()
+                            .map(TeachingSlot::fromDomain)
+                            .collect(toList()));
+        }
+    }
+
+    @Data
+    private static class TeachingSlot {
+        private final TeachingClass teachingClass;
+        private final Room room;
+        private final int pairNumber;
+        private final TimeCoordinates timeCoordinates;
+
+        public static TeachingSlot fromDomain(com.waytoodanny.timetable.TeachingSlot domain) {
+            return new TeachingSlot(
+                    TeachingClass.fromDomain(domain.getTeachingClass()),
+                    Room.fromDomain(domain.getRoom()),
+                    domain.getPairNumber(),
+                    TimeCoordinates.fromDomain(domain.getTimeCoordinates()));
+        }
+    }
+
+    @Data
+    private static class TeachingClass {
+        private final Subject subject;
+        private final Set<Teacher> teachers;
+        private final Set<StudentGroup> groups;
+        private final ClassType type;
+
+        public static TeachingClass
+        fromDomain(com.waytoodanny.timetable.genetic.domain.university.TeachingClass domain) {
+            return new TeachingClass(
+                    domain.getSubject(),
+                    domain.getTeachers(),
+                    domain.getGroups().stream().map(StudentGroup::fromDomain).collect(toSet()),
+                    domain.getType());
+        }
+    }
+
+    @Data
+    private static class TimeCoordinates {
+        private final int weekNumber;
+        private final Weekday day;
+        private final LocalTime start;
+        private final LocalTime end;
+
+        public static TimeCoordinates
+        fromDomain(com.waytoodanny.timetable.genetic.domain.TimeCoordinates domain) {
+            return new TimeCoordinates(
+                    domain.getWeekNumber(),
+                    domain.getDay(),
+                    domain.getStart(),
+                    domain.getEnd());
+        }
+    }
+
+    @Data
+    public static class StudentGroup {
+        private final String name;
+
+        public static StudentGroup
+        fromDomain(com.waytoodanny.timetable.genetic.domain.university.StudentGroup domain) {
+            return new StudentGroup(domain.getName());
+        }
+    }
+
+    @Data
+    private static class Room {
+        private final String name;
+
+        public static Room
+        fromDomain(com.waytoodanny.timetable.genetic.domain.university.Room domain) {
+            return new Room(domain.getName());
+        }
+    }
 }
