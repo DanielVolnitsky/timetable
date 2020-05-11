61bc3bdbac782ca21183483c05d5ab96b482ac50
Daniel <waytodanny@gmail.com>
2020-05-11 20:09:36 +0300

    refactor TeacherSingleClassAtTime and StudentGroupSingleClassAtTime

--- timetable-genetic-algorithm/src/main/java/com/waytoodanny/timetable/genetic/constraint/hard/StudentGroupSingleClassAtTime.java
+++ timetable-genetic-algorithm/src/main/java/com/waytoodanny/timetable/genetic/constraint/hard/StudentGroupSingleClassAtTime.java
@@ -11,24 +11,19 @@ import org.springframework.stereotype.Component;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
-import java.util.stream.Collectors;
 
-/**
- * No student group can attend more than one class at the same time.
- */
+import static java.util.stream.Collectors.toList;
+
 @Component
 public class StudentGroupSingleClassAtTime implements HardConstraint {
 
   @Override
   public FitnessFunction fitness(Chromosome chromosome, FitnessFunction initial) {
-    return chromosome.getScheduledClasses()
-        .values().stream()
-        .map(Chromosome.ScheduledClasses::getClasses)
-        .map(this::noClashes)
-        .filter(noClashes -> !noClashes)
-        .findAny()
-        .map(clashes -> initial)
-        .orElseGet(() -> initial.plus(this.weight()));
+    boolean allMatch = chromosome.scheduledClasses()
+        .stream()
+        .allMatch(scheduledClasses -> scheduledClasses.match(this::noClashes));
+
+    return allMatch ? initial.plus(this.weight()) : initial;
   }
 
   private boolean noClashes(List<SettledClass> classes) {
@@ -36,7 +31,7 @@ public class StudentGroupSingleClassAtTime implements HardConstraint {
         .map(SettledClass::getGeneticTeachingClass)
         .map(GeneticTeachingClass::involvedStudentGroups)
         .flatMap(Collection::stream)
-        .collect(Collectors.toList());
+        .collect(toList());
 
     return new HashSet<>(groups).size() == groups.size();
   }
